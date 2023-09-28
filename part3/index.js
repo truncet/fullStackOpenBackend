b0ded444030812cac041dd79dc259449ace0d4a6
Ranjit Nepal <theonlyeminem@gmail.com>
2023-09-28 10:24:40 +0300

    changes in index.js

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json());
 
 morgan.token('req-body', (req) => JSON.stringify(req.body));
@@ -130,7 +132,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
