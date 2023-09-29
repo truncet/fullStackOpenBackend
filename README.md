2c2eede42b106dcae1c4835dc0345d39abe97b53
Ranjit Nepal <theonlyeminem@gmail.com>
2023-09-29 12:29:03 +0300

    Exercise 3.11 done. deployed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fullStackOpenBackend
+
+The url for the exercise 3.10 is https://patient-snow-7157.fly.dev/
--- part3/index.js
+++ part3/index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+
+app.use(express.static('dist'))
 app.use(express.json());
 
 morgan.token('req-body', (req) => JSON.stringify(req.body));
@@ -135,4 +137,4 @@ app.post('/api/persons', (request, response) => {
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
+})  
