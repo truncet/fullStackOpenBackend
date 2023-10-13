326667bb18a358f54e192bfbc0a06baf7f66c2f4
Ranjit Nepal <theonlyeminem@gmail.com>
2023-10-14 02:20:00 +0300

    normal setup

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI = process.env === 'test'
+  ? process.env.Test_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
